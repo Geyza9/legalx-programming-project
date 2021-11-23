20d9c709f8002c5ea5f9d7bd411e9c1f56af543c
geza-lorincz <45147619+geza.lorincz@users.noreply.github.com>
2021-11-23 16:00:27 +0100

    import txt

---
+++ legalx-project/legalx-project/dataLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace legalxproject
+{
+    public class dataLoader
+    {
+        public dataLoader()
+        {
+        }
+
+        public void loadData()
+        {
+            List<String> data = File.ReadAllLines("lawyerDataBase.txt").ToList();
+
+            foreach (string line in data)
+            {
+                List<String> variables = 
+            }
+        }
+    }
+
+}

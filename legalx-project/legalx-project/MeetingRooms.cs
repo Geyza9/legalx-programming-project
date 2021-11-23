fa4209f42572ca629fb18ac3da8e93d95e3a9fa7
Just-In63 <justin.borggreve@gmail.com>
2021-11-23 20:30:46 +0100

    classes added

---
+++ legalx-project/legalx-project/MeetingRooms.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legalxproject
+{
+    //meeting room class
+    class MeetingRooms
+    {
+        public string name { get; set; }
+        public int capacity { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder meetingroomstats = new StringBuilder();
+            meetingroomstats.AppendLine("*********  Meeting Room Information *********");
+            meetingroomstats.AppendLine($"Meeting room name: {name}");
+            meetingroomstats.AppendLine($"Occupancy limit: {capacity}");
+
+            return meetingroomstats.ToString();
+        }
+    }
+}
---
+++ legalx-project/legalx-project/Receptionist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legalxproject
+{
+    //Fourth Child class of employee
+    class Receptionist : Employee
+    {
+        public Receptionist(int id, string fullname, DateTime joineddate, string otherexpertise) : base(id, fullname, joineddate, otherexpertise)
+        {
+            
+        }
+        public override string ToString()
+        {
+            StringBuilder Receptioniststats = new StringBuilder();
+            Receptioniststats.AppendLine(base.ToString());
+
+            return Receptioniststats.ToString();
+        }
+    }
+}
